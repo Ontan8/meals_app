2c5dd79315cbda41480ad0cd52d36c13f22fb30d
Ontan8 <tanishqpawar80@gmail.com>
2022-01-20 18:09:14 +0530

    added inkwell and navigator to the meals screen

--- lib/category_items.dart
+++ lib/category_items.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
   const CategoryItem(this.title, this.color, {Key? key}) : super(key: key);
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => CategoryMealsScreen(),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(15),
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(15),
-        ));
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+          padding: const EdgeInsets.all(15),
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [color.withOpacity(0.7), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15),
+          )),
+    );
   }
 }
