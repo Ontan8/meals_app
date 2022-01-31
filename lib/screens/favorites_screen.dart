a2462d166d63291160d056e0d90a6e37da90f927
Ontan8 <tanishqpawar80@gmail.com>
2022-01-31 22:15:57 +0530

    added main functionality

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/meal.dart';
+import '../widgets/meal_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({Key? key}) : super(key: key);
+  final List<Meal> favoriteMeals;
+  const FavoritesScreen(this.favoriteMeals, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Favorites'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('Add favorites dishes to display here!'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+            duration: favoriteMeals[index].duration,
+            imageUrl: favoriteMeals[index].imageUrl,
+            title: favoriteMeals[index].title,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
   }
 }
