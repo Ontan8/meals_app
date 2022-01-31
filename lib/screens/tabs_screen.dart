d156f938c572b513f3b539eee666e13cb3c08d4f
Ontan8 <tanishqpawar80@gmail.com>
2022-01-31 22:14:35 +0530

    pass fav meals list to fav meals screen widget

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/screens/categories_screen.dart';
 import 'package:flutter_complete_guide/screens/favorites_screen.dart';
 import 'package:flutter_complete_guide/widgets/main_drawer.dart';
+import '../models/meal.dart';
 
 //this uses the CategoriesScreen() to render so both this file and categories_screen.dart are important and in a sense the main page of the application.
 class TabsScreen extends StatefulWidget {
-  const TabsScreen({Key? key}) : super(key: key);
+  final List<Meal> favoriteMeals;
+  const TabsScreen(this.favoriteMeals, {Key? key}) : super(key: key);
 
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Map<String, Object>> pages = [
-    {'page': CategoriesScreen(), 'title': 'Categories'},
-    {'page': FavoritesScreen(), 'title': 'Your Favorites'}
-  ];
+  List<Map<String, Object>> pages = [];
   int selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    pages = [
+      {'page': CategoriesScreen(), 'title': 'Categories'},
+      {'page': FavoritesScreen(widget.favoriteMeals), 'title': 'Your Favorites'}
+    ];
+    super.initState();
+  }
+
   void selectPage(int index) {
     setState(() {
       selectedPageIndex = index;
